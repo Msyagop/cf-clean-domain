4e86a3e54f9255c7c46739c29caccaa22f680764
Msyagop <112186057+Msyagop@users.noreply.github.com>
2024-03-26 01:11:38 +0330

    Update updater.php

--- updater.php
+++ updater.php
@@ -17,7 +17,7 @@ function checkCloudflare($domain) {
 
 function main() {
     $inputFile = 'domains.txt';
-    $count = 900;
+    $count = 1500;
 
     $domains = file($inputFile, FILE_IGNORE_NEW_LINES);
 
